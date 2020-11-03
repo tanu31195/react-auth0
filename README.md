99c64b12907c72ca89e0e5c2df9bbc5c268f3d1b
Tanushka PC <tanu31195@users.noreply.github.com>
2020-11-03 22:27:20 +0530

    Added login and logout

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+npm i @auth0/auth0-react
+
+npm i react-json-pretty
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import LoginButton from "./Components/LoginButton";
+import LogoutButton from "./Components/LogoutButton";
+import Profile from "./Components/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div> Loading...</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+    <LoginButton />
+    <LogoutButton />
+    <Profile />
+    </>
   );
 }
 
